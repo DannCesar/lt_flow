59e52a73cac3b453ae8aabdcc96218e7ac281ec0
Daniel Cesar <danielcesartds@gmail.com>
2025-12-01 15:18:58 -0300

    chore: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+const config = {
+    content: [
+         "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme:{
+        extend: {},
+    },
+    plugins: []
+}
