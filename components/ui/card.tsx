786428feb2c532ba5b27c76b3e8044902a4b4b36
Daniel Cesar <danielcesartds@gmail.com>
2025-12-02 13:31:13 -0300

    Fix:  script and styles

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow",
+      "rounded-xl border bg-card  shadow bg-[#262332] border-[#2B2C42]",
       className
     )}
     {...props}
