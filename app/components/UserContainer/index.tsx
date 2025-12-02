b1c8601f2ad36ded48c3076b49c113b1351867b0
Daniel Cesar <danielcesartds@gmail.com>
2025-12-02 11:37:29 -0300

    Fix: width and height from Icon

--- app/components/UserContainer/index.tsx
+++ app/components/UserContainer/index.tsx
@@ -1,8 +1,8 @@
 import {CircleUserRoundIcon} from 'lucide-react'
 export default function UserContainer(){
     return(
-        <div className='flex items-center font-light text-white gap-3'>
-            <CircleUserRoundIcon  width={32} height={32}/>
+        <div className='flex items-center font-light  text-white gap-3'>
+            <CircleUserRoundIcon  width={28} height={28}/>
             <span>Daniel Cesar</span>
         </div>
     )
